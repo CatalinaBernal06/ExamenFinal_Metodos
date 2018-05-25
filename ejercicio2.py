649f44fa208963a5ba51dddb3753bec069abbd23
catalinabernal0-enfinal_metodos-gowmtp9u <catalinabernal0-enfinal_metodos-gowmtp9u@local>
2018-05-25 00:36:25 +0000

    Final

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,9 +2,20 @@
 # Complete el siguiente codigo para recorrer la lista `x` e imprima
 # los numeros impares y que pare de imprimir al encontrar un numero mayor a 800
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 
 x = np.int_(np.random.random(100)*1000)
-print(x)
+
+num = 0
+while(num<800):
+   for equis in x:
+        if(equis%2 != 0):
+            print(equis)
+            num = equis
+
 
 
 
--- ejercicio5.py
+++ ejercicio5.py
@@ -8,6 +8,32 @@
 # Prepare dos graficas con la solucion: de x vs y (xy.png), x vs. z (xz.png) 
 
 
+sig = 10
+beta = 2.67
+rho = 28.0
+t = 0
+x = 0.0
+y = 0.0
+z = 0.0
+delta_t = 0.001
+delta_x = 0.01
+T_max = 5.0
+
+def dx_dt(x, y):
+    return sig * (y - x)
+
+def dy_dt(x, y, z):
+    return rho * x - y - x*z
+
+def dz_dt(x, y, z):
+    return -beta * z + x * y
+
+x_l = list()
+y_l = list()
+z_l = list()
+
+
+
 
 
 
